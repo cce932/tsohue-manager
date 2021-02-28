f03cecaf8d1bf017f9b962ea8166432ed79d1149
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-02-28 22:55:43 +0800

    0228 enhance: remove redundant code on deleteData

--- src/middlewares/deleteData/deleteEmployee.js
+++ src/middlewares/deleteData/deleteEmployee.js
@@ -6,23 +6,14 @@ const deleteEmployee = (store) => (next) => (action) => {
 
   if (type === DELETE_EMPLOYEE) {
     new Promise((resolve, reject) => {
-      if (payload.id.length > 1) {
-        const ids = payload.id
+      const ids = payload.id
 
-        ids.map((id, index) =>
-          DeleteService.deleteEmployee(id)
-            .then(() => {
-              index === ids.length - 1 && resolve() // (待改善) 要等選取的多個id都delted 才該去getAllMember 理想上要用await
-            })
-            .catch((error) => reject(error))
-        )
-      } else {
-        DeleteService.deleteEmployee(payload.id)
-          .then(() => resolve())
-          .catch((error) => reject(error))
-      }
+      ids.map((id) =>
+        DeleteService.deleteEmployee(id).catch((error) => reject(error))
+      )
+      resolve()
     }).then(
-      (response) => {
+      () => {
         console.info(`Delete employee ID: ${payload.id} successful`)
         // store.dispatch(getAllEmployees())
         window.location.reload() // 解決刪除後 "清除篩選壞掉"的bug
--- src/middlewares/deleteData/deleteMember.js
+++ src/middlewares/deleteData/deleteMember.js
@@ -4,27 +4,15 @@ import DeleteService from "services/delete.service"
 const deleteMember = (store) => (next) => (action) => {
   const { type, payload } = action
   if (type === DELETE_MEMBER) {
-
     new Promise((resolve, reject) => {
-      if (payload.id.length > 1) {
-        const ids = payload.id
+      const ids = payload.id
 
-        ids.map((id, index) =>
-          DeleteService.deleteMember(id)
-            .then(() => {
-              index === ids.length - 1 && resolve() // (待改善) 要等選取的多個id都delted 才該去getAllMember 理想上要用await
-            })
-            .catch((error) => reject(error))
-        )
-      } else {
-
-        DeleteService.deleteMember(payload.id)
-          .then(() => resolve())
-          .catch((error) => reject(error))
-      }
-      
+      ids.map((id) =>
+        DeleteService.deleteMember(id).catch((error) => reject(error))
+      )
+      resolve() // 所有都delete之後才resolve
     }).then(
-      (response) => {
+      () => {
         console.info(`Delete member ID: ${payload.id} successful`)
         // store.dispatch(getAllMembers())
         window.location.reload() // 解決刪除後 "清除篩選壞掉"的bug
