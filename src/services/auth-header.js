ba55d35828d374df13d7ed8f95cc70d17525ad5a
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-03-04 20:54:01 +0800

    0303 Fix: non-login user access pages will get 400 EMPTY_TOKEN and redirect to /login

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -8,7 +8,7 @@ const authHeader = () => {
     if ( user && user.token) {
         return { Authorization: user.token }
     } else {
-        return {}
+        return { Authorization: "" }
     }
 }
 
