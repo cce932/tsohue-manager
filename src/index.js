b1f5e678df4ec82b887fbf99a0f1caf715ece06b
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-05-11 12:52:49 +0800

    0511 Feature: add theme for styled-components and adjust color

--- src/index.js
+++ src/index.js
@@ -1,25 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 import store from "./store"
 import { GlobalDialog } from "pages/GlobalDialog"
+import { ThemeProvider, css } from "styled-components"
+
+const theme = {
+  primeColor: "rgb(52, 58, 64)",
+  secondaryColor: "rgb(86, 90, 95)",
+  thirdColor: "rgb(123, 127, 131)",
+  forthColor: "rgb(161, 163, 165)",
+  fifthColor: "rgb(232, 235, 240)",
+  sixthColor: "rgb(249, 250, 252)",
+  accentColor: "rgb(231, 104, 69)",
+  accentLighterColor: "rgb(253, 221, 132)",
+  accentLighter2Color: "rgb(248, 203, 80)",
+  accentDeeperColor: "rgb(165, 90, 70)",
+  normalColor: "#9e8568",
+  lowfatColor: "#8093b5",
+  meatColor: "#f09797",
+  vageColor: "#7ca390",
+  font: css`
+    font-family: sans-serif;
+    text-decoration: none;
+    letter-spacing: 0.03em;
+  `,
+  defaultShadow: "0px 0px 15px rgba(183, 186, 191, 0.3)",
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalDialog>
         <App />
       </GlobalDialog>
-    </>
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root")
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/shared/style/color.js
+++ src/shared/style/color.js
@@ -2,13 +2,17 @@ const color = {
   prime: "rgb(52, 58, 64)",
   secondary: "rgb(86, 90, 95)",
   third: "rgb(123, 127, 131)",
-  forth: "rgb(205, 211, 216)",
+  forth: "rgb(161, 163, 165)",
   fifth: "rgb(232, 235, 240)",
   sixth: "rgb(249, 250, 252)",
   accent: "rgb(231, 104, 69)",
   accentLighter: "rgb(253, 221, 132)",
   accentLighter2: "rgb(248, 203, 80)",
   accentDeeper: "rgb(165, 90, 70)",
+  normal: "#9e8568",
+  lowfat: "#8093b5",
+  meat: "#f09797",
+  vage: "#7ca390",
 }
 
 export default color
